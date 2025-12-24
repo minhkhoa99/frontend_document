b159f90d8c6bf21684c58927db4b26e4ad53a514
khoatm <khoatm@vtvlive.vn>
2025-12-24 10:21:03 +0700

    update

--- src/categories/category-page/dto/update-category-page.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCategoryPageDto } from './create-category-page.dto';
-
-export class UpdateCategoryPageDto extends PartialType(CreateCategoryPageDto) {}
